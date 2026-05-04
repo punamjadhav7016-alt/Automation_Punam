adcb0a4b80627c4d52deb7eef42c43873a8692d9
punamjadhav <punamjadhav7016@gmai.com>
2026-05-04 21:34:50 +0530

    CRM Framework Script

---
+++ src/test/java/Com/CRM/TestCases/HomepageTestCase.java
@@ -0,0 +1,36 @@
+package Com.CRM.TestCases;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import Com.CRM.pages.Homepages;
+
+public class HomepageTestCase extends Baseclass {
+	
+	@Test(priority=1)
+	public void verifyURL()
+	{
+	String act=hp.AppUrl();
+	Assert.assertTrue(act.contains("crm"),"Fail");
+	System.out.println("UrlMatch ");
+	}
+	@Test(priority=2)
+	public void verifyTitle()
+	
+	{
+		String act=hp.getTitle();
+		Assert.assertTrue(act.contains("Customer"),"Test Fail");
+		System.out.println("TitleMatch ");
+
+	}
+	@Test(priority=3)
+	public void verifySiginin()
+	
+	{
+		String nextpageURL=hp.clicklogin();
+		Assert.assertTrue(nextpageURL.contains("login"),"Test Fail");
+		System.out.println("pass ");
+
+	}
+
+}
